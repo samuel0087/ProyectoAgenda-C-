e401f6c800da973e12703afeddaf80848d07dc62
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2026-01-16 15:11:51 -0300

    se ralizaron validaciones

--- AgendaDeContactos/AppAgenda/FrmContactos.Designer.cs
+++ AgendaDeContactos/AppAgenda/FrmContactos.Designer.cs
@@ -46,9 +46,10 @@
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(78, 35);
+            this.lblTitulo.Location = new System.Drawing.Point(104, 43);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(162, 25);
+            this.lblTitulo.Size = new System.Drawing.Size(204, 31);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Nuevo contacto";
             // 
@@ -56,9 +57,10 @@
             // 
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(21, 97);
+            this.lblNombre.Location = new System.Drawing.Point(28, 119);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(59, 16);
+            this.lblNombre.Size = new System.Drawing.Size(73, 20);
             this.lblNombre.TabIndex = 0;
             this.lblNombre.Text = "Nombre:";
             // 
@@ -66,9 +68,10 @@
             // 
             this.lblApellido.AutoSize = true;
             this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblApellido.Location = new System.Drawing.Point(21, 147);
+            this.lblApellido.Location = new System.Drawing.Point(28, 181);
+            this.lblApellido.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblApellido.Name = "lblApellido";
-            this.lblApellido.Size = new System.Drawing.Size(60, 16);
+            this.lblApellido.Size = new System.Drawing.Size(73, 20);
             this.lblApellido.TabIndex = 0;
             this.lblApellido.Text = "Apellido:";
             // 
@@ -76,39 +79,47 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(21, 201);
+            this.label4.Location = new System.Drawing.Point(28, 247);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.Size = new System.Drawing.Size(78, 20);
             this.label4.TabIndex = 0;
             this.label4.Text = "Telefono:";
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(104, 96);
+            this.txtNombre.Location = new System.Drawing.Point(139, 118);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(176, 20);
+            this.txtNombre.Size = new System.Drawing.Size(233, 22);
             this.txtNombre.TabIndex = 0;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
             // 
             // txtApellido
             // 
-            this.txtApellido.Location = new System.Drawing.Point(104, 146);
+            this.txtApellido.Location = new System.Drawing.Point(139, 180);
+            this.txtApellido.Margin = new System.Windows.Forms.Padding(4);
             this.txtApellido.Name = "txtApellido";
-            this.txtApellido.Size = new System.Drawing.Size(176, 20);
+            this.txtApellido.Size = new System.Drawing.Size(233, 22);
             this.txtApellido.TabIndex = 1;
+            this.txtApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellido_KeyPress);
             // 
             // txtTelefono
             // 
-            this.txtTelefono.Location = new System.Drawing.Point(104, 200);
+            this.txtTelefono.Location = new System.Drawing.Point(139, 246);
+            this.txtTelefono.Margin = new System.Windows.Forms.Padding(4);
             this.txtTelefono.Name = "txtTelefono";
-            this.txtTelefono.Size = new System.Drawing.Size(176, 20);
+            this.txtTelefono.Size = new System.Drawing.Size(233, 22);
             this.txtTelefono.TabIndex = 2;
+            this.txtTelefono.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTelefono_KeyPress);
             // 
             // btnCancelar
             // 
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelar.Location = new System.Drawing.Point(42, 255);
+            this.btnCancelar.Location = new System.Drawing.Point(56, 314);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(93, 33);
+            this.btnCancelar.Size = new System.Drawing.Size(124, 41);
             this.btnCancelar.TabIndex = 3;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -117,21 +128,24 @@
             // btnGuardar
             // 
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardar.Location = new System.Drawing.Point(198, 255);
+            this.btnGuardar.Location = new System.Drawing.Point(264, 314);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(93, 33);
+            this.btnGuardar.Size = new System.Drawing.Size(124, 41);
             this.btnGuardar.TabIndex = 4;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // lblErrorNombre
             // 
             this.lblErrorNombre.AutoSize = true;
             this.lblErrorNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblErrorNombre.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorNombre.Location = new System.Drawing.Point(101, 80);
+            this.lblErrorNombre.Location = new System.Drawing.Point(135, 98);
+            this.lblErrorNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblErrorNombre.Name = "lblErrorNombre";
-            this.lblErrorNombre.Size = new System.Drawing.Size(0, 13);
+            this.lblErrorNombre.Size = new System.Drawing.Size(0, 17);
             this.lblErrorNombre.TabIndex = 3;
             // 
             // lblErrorApellido
@@ -139,9 +153,10 @@
             this.lblErrorApellido.AutoSize = true;
             this.lblErrorApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblErrorApellido.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorApellido.Location = new System.Drawing.Point(101, 130);
+            this.lblErrorApellido.Location = new System.Drawing.Point(135, 160);
+            this.lblErrorApellido.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblErrorApellido.Name = "lblErrorApellido";
-            this.lblErrorApellido.Size = new System.Drawing.Size(0, 13);
+            this.lblErrorApellido.Size = new System.Drawing.Size(0, 17);
             this.lblErrorApellido.TabIndex = 3;
             // 
             // lblErrorTelefono
@@ -149,16 +164,17 @@
             this.lblErrorTelefono.AutoSize = true;
             this.lblErrorTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblErrorTelefono.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorTelefono.Location = new System.Drawing.Point(101, 184);
+            this.lblErrorTelefono.Location = new System.Drawing.Point(135, 226);
+            this.lblErrorTelefono.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblErrorTelefono.Name = "lblErrorTelefono";
-            this.lblErrorTelefono.Size = new System.Drawing.Size(0, 13);
+            this.lblErrorTelefono.Size = new System.Drawing.Size(0, 17);
             this.lblErrorTelefono.TabIndex = 3;
             // 
-            // FormularioContactos
+            // FrmContactos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 315);
+            this.ClientSize = new System.Drawing.Size(445, 388);
             this.Controls.Add(this.lblErrorTelefono);
             this.Controls.Add(this.lblErrorApellido);
             this.Controls.Add(this.lblErrorNombre);
@@ -172,7 +188,8 @@
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.lblTitulo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Name = "FormularioContactos";
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmContactos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormularioContactos";
             this.Load += new System.EventHandler(this.FrmContactos_Load);
--- AgendaDeContactos/AppAgenda/FrmContactos.cs
+++ AgendaDeContactos/AppAgenda/FrmContactos.cs
@@ -1,4 +1,5 @@
 ﻿using Dominio;
+using Negocio;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,7 @@ namespace AppAgenda
     public partial class FrmContactos : Form
     {
         private Contacto contacto = null;
+        ContactoNegocio cNegocio = new ContactoNegocio();
 
         public FrmContactos()
         {
@@ -47,6 +49,156 @@ namespace AppAgenda
             txtNombre.Text = contacto.Nombre;
             txtApellido.Text = contacto.Apellido;
             txtTelefono.Text = contacto.Telefono;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if(contacto == null)
+            {
+                contacto = new Contacto();
+            }
+
+            try
+            {
+                if(contacto.IdContacto == 0)
+                {
+                    if (cNegocio.ExisteTelefono(txtTelefono.Text))
+                    {
+                        DialogResult result = MessageBox.Show("El telefono ya esta registrado, Desea guardarlo igualmente?", "Atencion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (result == DialogResult.No)
+                        {
+                            return;
+                        }
+                    }
+                }
+
+                if (!ValidarFormulario())
+                {
+                    MessageBox.Show("Verifique los campor por favor.");
+                    return;
+                }
+
+                contacto.Nombre = txtNombre.Text;
+                contacto.Apellido = txtApellido.Text;
+                contacto.Telefono = txtTelefono.Text;
+
+                DialogResult resultado = MessageBox.Show("Esta seguro de que desea realizar esta operacion?", "Confirmacion", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (resultado == DialogResult.No) 
+                {
+                    return;
+                }
+
+
+                if(contacto.IdContacto != 0)
+                {
+                    cNegocio.Modificar(contacto);
+                }
+                else
+                {
+                    cNegocio.Agregar(contacto);
+                }
+
+                MessageBox.Show("Operacion realizada con exito");
+
+                this.DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch
+            {
+                MessageBox.Show("No se pudo guardar el registro correctamente");
+            }
+   
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ' ')
+            {
+                e.Handled = true;
+            }
+
+            if( txtNombre.Text != "")
+            {
+                if (txtNombre.Text[txtNombre.Text.Length - 1] == ' ' && e.KeyChar == ' ')
+                {
+                    e.Handled = true;
+                }
+            }
+
+            if(txtNombre.Text == "" && e.KeyChar == ' ')
+            {
+                e.Handled= true;
+            }
+        }
+
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ' ')
+            {
+                e.Handled = true;
+            }
+
+            if (txtApellido.Text != "")
+            {
+                if (txtApellido.Text[txtApellido.Text.Length - 1] == ' ' && e.KeyChar == ' ')
+                {
+                    e.Handled = true;
+                }
+            }
+
+            if (txtApellido.Text == "" && e.KeyChar == ' ')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtTelefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '+')
+            {
+                e.Handled = true;
+            }
+
+            if(txtTelefono.Text != "" && e.KeyChar == '+')
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private bool ValidarFormulario()
+        {
+            bool estado = true;
+            lblNombre.Text = string.Empty;
+            lblApellido.Text = string.Empty;
+            lblErrorTelefono.Text = string.Empty;
+
+            if (string.IsNullOrEmpty(txtNombre.Text)) 
+            {
+                lblNombre.Text = "Campo oblgatorio";
+                estado = false;
+            }
+
+            if (string.IsNullOrEmpty(txtApellido.Text))
+            {
+                lblApellido.Text = "Campo obligatorio";
+                estado = false;
+            }
+
+            if (string.IsNullOrEmpty(txtTelefono.Text))
+            {
+                lblErrorTelefono.Text = "Campo obligatorio";
+
+                if(txtTelefono.Text == "+")
+                {
+                    lblErrorTelefono.Text = "Debe contener numeros";
+                }
+                estado = false;
+            }
+
+
+            return estado;
         }
     }
 }
